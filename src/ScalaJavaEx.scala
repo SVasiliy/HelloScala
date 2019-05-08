b3ca5663817af7eb426ffa4adbee634097e36d69
vs <vasiliy.sharapov@gmail.com>
2019-05-07 20:59:35 -0500

    test scala class

--- src/ScalaJavaEx.scala
+++ src/ScalaJavaEx.scala
@@ -2,10 +2,17 @@ import java.time.LocalDate
 import java.time.format.{DateTimeFormatter, FormatStyle}
 import java.util.Locale
 
+class Student(var name: String, var rollNo: Int) {
+  override def toString: String = s"($name, $rollNo)"
+}
+
 object ScalaJavaEx {
   def main(args: Array[String]): Unit ={
     val now = LocalDate.now
     val df = DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL).withLocale(Locale.FRANCE)
     println(now format df)
+
+    val test = new Student("sdf", 4)
+    println(test toString)
   }
 }

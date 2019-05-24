d8d10a62aa1bba76df0edad49ba00b15ce2fc1e4
SVasiliy <vasiliy.sharapov@gmail.com>
2019-05-23 19:16:52 -0500

    test nullPointerEx in scala

--- src/Hello.scala
+++ src/Hello.scala
@@ -1,9 +1,9 @@
 object Hello extends App {
+
   println("Hello, World!")
+
   def add(x: Int, y: Int): Int = x + y
   println(add(1, 2)) // 3
-
-
 
   val builder = new StringBuilder
 
@@ -12,6 +12,13 @@ object Hello extends App {
   def z = { builder += 'z'; 3 }
 
   z + y + x + z + y + x
-
   println(builder.result())
+
+  // test null value
+  def maybeItWillReturnSomething(flag: Boolean): Option[String] = {
+    if (flag) Some("Found value") else null
+  }
+
+  println(maybeItWillReturnSomething(false).getClass)
+
 }

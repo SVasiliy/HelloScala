a0657a83fc52e4051748e7f05fe0a6c7d5d9827e
SVasiliy <vasiliy.sharapov@gmail.com>
2019-05-23 19:12:51 -0500

    tast lazy val

--- src/Hello.scala
+++ src/Hello.scala
@@ -2,4 +2,16 @@ object Hello extends App {
   println("Hello, World!")
   def add(x: Int, y: Int): Int = x + y
   println(add(1, 2)) // 3
+
+
+
+  val builder = new StringBuilder
+
+  val x = { builder += 'x'; 1 }
+  lazy val y = { builder += 'y'; 2 }
+  def z = { builder += 'z'; 3 }
+
+  z + y + x + z + y + x
+
+  println(builder.result())
 }

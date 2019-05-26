7ae55dd40652680f9b6208ba458c0d35734d318b
SVasiliy <vasiliy.sharapov@gmail.com>
2019-05-25 22:26:55 -0500

    map test +

--- src/MapTest.scala
+++ src/MapTest.scala
@@ -6,7 +6,19 @@ object MapTest {
 
     val mapValues = myMap.values
 
-    println(mapValues) // function
+    println(mapValues)
+    println(myMap("MI")) // returns just value
+
+    // Map keys may be of mixed type:
+    val myMap2 = Map("Ann Arbor" → "MI", 49931 → "MI")
+
+    // Map equivalency is independent of order:
+    val myMap3 =
+      Map("MI" → "Michigan", "OH" → "Ohio", "WI" → "Wisconsin", "IA" → "Iowa")
+    val myMap4 =
+      Map("WI" → "Wisconsin", "MI" → "Michigan", "IA" → "Iowa", "OH" → "Ohio")
+    println (myMap3.equals(myMap4)) // true
+    println (myMap3 == myMap4) // true
 
   }
 
